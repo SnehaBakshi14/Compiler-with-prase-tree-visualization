c449bb378d518c39860905f365b8f6ca6b1f3105
SnehaBakshi14 <snehabakshi1405@gmail.com>
2025-05-26 19:42:36 +0530

    Enhance parse tree visualization and error handling

--- src/components/visualizations/ErrorList.tsx
+++ src/components/visualizations/ErrorList.tsx
@@ -10,8 +10,8 @@ const ErrorList: React.FC<ErrorListProps> = ({ errors }) => {
     return (
       <div className="h-full flex items-center justify-center">
         <div className="text-center">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12 mx-auto text-green-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+          <svg xmlns="http://www.w3.org/2000/svg\" className="h-12 w-12 mx-auto text-green-500\" fill="none\" viewBox="0 0 24 24\" stroke="currentColor">
+            <path strokeLinecap="round\" strokeLinejoin="round\" strokeWidth={2} d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
           </svg>
           <h3 className="text-lg font-medium text-gray-900 mt-3">No errors found</h3>
           <p className="text-gray-500 mt-1">Your code compiled successfully without any errors.</p>
@@ -43,8 +43,8 @@ const ErrorList: React.FC<ErrorListProps> = ({ errors }) => {
               <div className="flex items-start">
                 <div className="flex-shrink-0">
                   {error.severity === 'error' ? (
-                    <svg className="h-5 w-5 text-red-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                      <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z" clipRule="evenodd" />
+                    <svg className="h-5 w-5 text-red-400\" xmlns="http://www.w3.org/2000/svg\" viewBox="0 0 20 20\" fill="currentColor">
+                      <path fillRule="evenodd\" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z\" clipRule="evenodd" />
                     </svg>
                   ) : (
                     <svg className="h-5 w-5 text-yellow-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
